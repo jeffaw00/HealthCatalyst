4bdcb2c54aae1649f3f71b427c6317d77bd9baaa
gituser <jtwilliams1995@msn.com>
2016-08-20 13:07:10 -0600

    Initial Commit

---
+++ HealthCatalystPeopleSearch/Migrations/201608201855447_AddTable.cs
@@ -0,0 +1,32 @@
+namespace HealthCatalystPeopleSearch.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PersonId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                        Age = c.Int(nullable: false),
+                        PicName = c.String(),
+                        PicLocation = c.String(),
+                        Interests = c.String(),
+                    })
+                .PrimaryKey(t => t.PersonId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ HealthCatalystPeopleSearch/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthCatalystPeopleSearch.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string PicName { get; set; }
+        public string PicLocation { get; set; }
+        public string Interests { get; set; }
+    }
+
+    public class PersonContext : DbContext
+    {
+        public PersonContext()
+            : base("name=PersonContext")
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
