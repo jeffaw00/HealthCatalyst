c92d5a4937df15ecccf526e236ee24ed10100e6d
gituser <jtwilliams1995@msn.com>
2016-08-22 22:57:06 -0600

    added theme and twitter type ahead

--- HealthCatalystPeopleSearch/App_Start/BundleConfig.cs
+++ HealthCatalystPeopleSearch/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace HealthCatalystPeopleSearch
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/hcScripts.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/hcStyles.css"));
         }
     }
 }
